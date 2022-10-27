34b40ca2c080475b13755fe937960aad016f7830
shafi <asshafi15@gmail.com>
2022-10-27 23:25:30 +0600

    side bar added with content

--- src/Layout/Main.js
+++ src/Layout/Main.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Header from '../Components/Header/Header';
-import Footer from '../Footer/Footer';
+import Footer from '../Components/Footer/Footer';
 
 const Main = () => {
     return (
