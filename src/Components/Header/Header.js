6977ea70e280faf043e2ffff8a58c66f0c6e089e
shafi <asshafi15@gmail.com>
2022-10-27 22:45:17 +0600

    firebase install and login  authentication added

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,18 +1,42 @@
-import React from 'react';
-import { Link, NavLink } from 'react-router-dom';
+import React, { useContext, useEffect, useState } from 'react';
+import { Link, NavLink, useLocation } from 'react-router-dom';
 import './Header.css';
-
+import { AuthContext } from '../../Contexts/UserContext';
+import LocalStore from '../../Storage';
 
 const Header = () => {
-   
-   
+    const [isMobile, setIsMobile] = useState(false);
+   //Checking screen size 
+   const handleResize = () => {
+    if (window.innerWidth < 768) {
+        setIsMobile(true)
+    } else {
+        setIsMobile(false)
+    }
+}
 
+// create an event listener
+ useEffect(() => {
+    window.addEventListener("resize", handleResize);
+    handleResize();
+    let dark = LocalStore('darkmode')===true;
+    if(dark){
+        document.querySelector("body").classList.add('dark');
+    }else{
+        document.querySelector("body").classList.remove('dark');
+    }
+})
+   
+    const { user } = useContext(AuthContext);
+    const location = useLocation();
     let activeClassName = "nav-link px-2 link-dark active-link nav-item fw-bold";
     let inActiveClass = "nav-link px-2 link-dark nav-item text-white fw-bold";
-  
+    let homeClass = "py-3 border-bottom active top-0 start-0";
+    let otherClass = "py-3 border-bottom";
+
     return (
         
-    <header>
+    <header className={location.pathname !== '/' ? otherClass : (isMobile ? otherClass : homeClass)}>
         <nav id="nav" class="navbar navbar-expand-md fixed-top ">
             <div class="container-fluid d-flex justify-content-between">
                 <div class="d-flex align-items-center mb-md-0 me-md-auto">
@@ -36,6 +60,16 @@ const Header = () => {
                         <li><NavLink to="/blog" className={(({ isActive }) => isActive ? activeClassName : inActiveClass)} end>Blog</NavLink></li>
                 
                     </ul>
+                    {ToggleDarkMode()}
+
+                    {user ? <div className="text-end d-flex mt-2 align-items-center justify-content-center">
+                        <Link to="#" className="d-block link-dark text-decoration-none"><img src={user.photoURL} alt={user.displayName} title={user.displayName} width="32" height="32" className="rounded-circle" /></Link>
+                        <Link to="/logout" className="btn btn-outline-danger ms-3">Logout</Link>
+                    </div> :
+                        <div className="text-end mt-2">
+                            <Link to="/login" className="btn btn-outline-primary me-2">Login</Link>
+                            <Link to="/signup" className="btn btn-primary">Sign-up</Link>
+                        </div>}
                 </div>
             </div>
         </nav>
@@ -44,5 +78,22 @@ const Header = () => {
         
     );
 };
+const ToggleDarkMode = () => {
+
+    const [darkmode, setDarkmode] = useState(LocalStore('darkmode'));
+    const handleDarkmode = (e) => {
+        setDarkmode(e.target.checked);
+        LocalStore('darkmode', e.target.checked);
+    }
+
+    return (
+        <div className='text-center col-12 col-md-auto mb-2 justify-content-center mb-md-0 mt-2'>
+            <div className="form-check form-switch mx-auto d-inline-block">
+                <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" checked={darkmode} onChange={handleDarkmode} />
+                <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Darkmode</label>
+            </div>
+        </div>
+    );
+}
 
 export default Header;
---
+++ src/Firebase/Firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDmKm2I-lFjw9gbVJe2F4KMzqMu2s0R1ZU",
+  authDomain: "sintag-private-limited.firebaseapp.com",
+  projectId: "sintag-private-limited",
+  storageBucket: "sintag-private-limited.appspot.com",
+  messagingSenderId: "117122417489",
+  appId: "1:117122417489:web:bbcd80bcc6a9f52cbe4970"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default FirebaseApp;
